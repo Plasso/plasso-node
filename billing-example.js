4b69a9061776878a5d7943cb9485fc2afdd18434
Michael Osuna Jr <mike@plasso.com>
2017-09-11 18:48:04 +0000

    update example code to match documentation

--- billing-example.js
+++ billing-example.js
@@ -9,7 +9,13 @@ app.use('/protected', (req, res) => {
   res.end('super secret site');
 });
 app.use((req, res) => {
-  res.end('public');
+	var html = `
+<a href="https://plasso.com/s/abc123" class="plo-button">Sign Up</a>
+<a href="https://plasso.com/s/abc123/login" class="plo-button">Login</a>
+<script src='https://plasso.co/embed/v3/e.js'></script>
+`;
+  res.end(html);
+
 });
 
 app.listen(3131);
